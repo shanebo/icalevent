0c1bfeb51229a64ca34a20f98c2d9e10c4e06594
Shane <shane@steadymade.com>
2013-01-30 12:25:19 -0600

    updated and cleaned up example

--- example/example.js
+++ example/example.js
@@ -1,42 +1,28 @@
 
 var icalevent = require('icalevent');
 var tzone = require('tzone');
-//var info = new Date().tzinfo();
-
-
-var loc = tzone.getLocation(new Date());
-console.log(loc);
+var http = require('http');
 
 
 var event = new icalevent();
-event.set('summary', 'Listen to Renewing Your Mind on REFNET');
+
+event.set('summary', 'Pointless meeting');
 event.set('offset', new Date().getTimezoneOffset());
-event.set('start', '2012-02-01T02:00:00-05:00');
-event.set('end', '2012-02-01T02:26:25-05:00');
-event.set('url', 'http://localhost:3100/');
-event.set('organizer', {name: 'REFNET', email: 'help@refnet.fm'});
+event.set('start', '2014-07-01T02:00:00-05:00');
+event.set('end', '2014-07-01T02:30:00-05:00');
+event.set('url', 'http://google.com');
+event.set('organizer', {name: 'shanebo', email: 'foo@email.com'});
 
-console.log(event.toFile());
+var file = event.toFile();
+console.log(file);
 
-
-var http = require('http');
 
 http.createServer(function(request, response){
 
-	var event = new icalevent();
-	event.set('summary', 'Listen to Renewing Your Mind on REFNET');
-	event.set('offset', new Date().getTimezoneOffset());
-	event.set('start', '2012-02-01T02:00:00-05:00');
-	event.set('end', '2012-02-01T02:26:25-05:00');
-	event.set('url', 'http://localhost:3100/');
-	event.set('organizer', {name: 'REFNET', email: 'help@refnet.fm'});
-	
 	response.writeHead(200, {'Content-Type': 'text/calendar'});
-	console.log(event.toFile());
-	response.end(event.toFile());
+	response.end(file);
 
 }).listen(9999, '127.0.0.1');
 
 
 console.log('Server running at http://127.0.0.1:9999/');
-

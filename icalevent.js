162b0e2fdc2e743e7adc5cdbb3a8d95c07e5f3ec
Christian Dittrich <christian.dittrich@creatale.de>
2014-07-29 13:03:55 +0200

    escape ',', ';' and '\'

--- icalevent.js
+++ icalevent.js
@@ -1,6 +1,10 @@
 var tzone = require('tzone');
 var moment = require('moment');
 
+var escapeText = function(text) {
+	return text.toString().replace(/([,;\\])/g, "\\$1");
+}
+
 var iCalendar = function(calendar, events) {
 	this.calendar = {}
 	this.calendar.id = '-//iCalEvent.js v0.3//EN';
@@ -42,15 +46,15 @@ iCalendar.prototype = {
 
 		result += 'BEGIN:VCALENDAR\r\n';
 		result += 'VERSION:2.0\r\n';
-		result += 'PRODID:' + this.calendar.id + '\r\n';
-		if (this.calendar.method) result += 'METHOD:' + this.calendar.method.toUpperCase() + '\r\n';
+		result += 'PRODID:' + escapeText(this.calendar.id) + '\r\n';
+		if (this.calendar.method) result += 'METHOD:' + escapeText(this.calendar.method.toUpperCase()) + '\r\n';
 		if (this.calendar.name) {
-			result += 'NAME:' + this.calendar.name + '\r\n';
-			result += 'X-WR-CALNAME:' + this.calendar.name + '\r\n';
+			result += 'NAME:' + escapeText(this.calendar.name) + '\r\n';
+			result += 'X-WR-CALNAME:' + escapeText(this.calendar.name) + '\r\n';
 		}
 		if (this.calendar.description) {
-			result += 'DESCRIPTION:' + this.calendar.description + '\r\n';
-			result += 'X-WR-CALDESC:' + this.calendar.description + '\r\n';
+			result += 'DESCRIPTION:' + escapeText(this.calendar.description) + '\r\n';
+			result += 'X-WR-CALDESC:' + escapeText(this.calendar.description) + '\r\n';
 		}
 		
 		for(var i = 0; i < this.events.length; i++) {
@@ -112,30 +116,30 @@ iCalEvent.prototype = {
 		var result = '';
 
 		result += 'BEGIN:VEVENT\r\n';
-		result += 'UID:' + this.event.uid + '\r\n';
+		result += 'UID:' + escapeText(this.event.uid) + '\r\n';
 		result += 'DTSTAMP:' + this.format(new Date()) + '\r\n';
 
-		if (this.event.status)			result += 'STATUS:' + this.event.status.toUpperCase() + '\r\n';
+		if (this.event.status)			result += 'STATUS:' + escapeText(this.event.status.toUpperCase()) + '\r\n';
 		if (this.event.start)			result += 'DTSTART;TZID=' + this.event.timezone + (this.event.allDay ? ';VALUE=DATE' : '') + ':' + this.format(this.event.start, this.event.allDay) + '\r\n';
 		if (this.event.end)				result += 'DTEND;TZID=' + this.event.timezone + (this.event.allDay ? ';VALUE=DATE' : '') + ':' + this.format(this.event.end, this.event.allDay) + '\r\n';
-		if (this.event.summary)			result += 'SUMMARY:' + this.event.summary + '\r\n';
-		if (this.event.description)		result += 'DESCRIPTION:' + this.event.description + '\r\n';
+		if (this.event.summary)			result += 'SUMMARY:' + escapeText(this.event.summary) + '\r\n';
+		if (this.event.description)		result += 'DESCRIPTION:' + escapeText(this.event.description) + '\r\n';
 		if (this.event.organizer) {
-			result += 'ORGANIZER'
+			result += 'ORGANIZER';
 			if (this.event.organizer.name) {
-				result += ';CN="' + this.event.organizer.name; + '"';
+				result += ';CN=' + escapeText(this.event.organizer.name);
 			}
-			result += ':'
+			result += ':';
 			if (this.event.organizer.email) {
-				result += 'mailto:' + this.event.organizer.email
+				result += 'mailto:' + this.event.organizer.email;
 			}
 			result += '\r\n';
 		}
-		if (this.event.location)		result += 'LOCATION:' + this.event.location + '\r\n';
+		if (this.event.location)		result += 'LOCATION:' + escapeText(this.event.location) + '\r\n';
 		if (this.event.url)				result += 'URL;VALUE=URI:' + this.event.url + '\r\n';
 		if (this.event.attendees) {
 			this.event.attendees.forEach(function(attendee){
-				result += 'ATTENDEE;RSVP=TRUE;ROLE=REQ-PARTICIPANT;PARTSTAT=NEEDS-ACTION;CN=' + attendee.name + ':MAILTO:' + attendee.email + '\r\n';
+				result += 'ATTENDEE;RSVP=TRUE;ROLE=REQ-PARTICIPANT;PARTSTAT=NEEDS-ACTION;CN=' + escapeText(attendee.name) + ':MAILTO:' + attendee.email + '\r\n';
 			});
 		}
 
